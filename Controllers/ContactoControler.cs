8caa353d62d80028cb275f89f7701605c0e3e332
alexander abanto <alexanderabanto19@hotmail.com>
2022-04-30 01:07:08 -0500

    nosotrosmashtml

--- Controllers/ContactoControler.cs
+++ Controllers/ContactoControler.cs
@@ -2,11 +2,17 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ControlWavi.Models;
 
 namespace ControlWavi.Controllers
 {
-    public class ContactoControler
+    public class ContactoController: Controller
     {
         
+
+      
+
+
     }
 }
